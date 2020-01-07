88e80b6236fd6ad82be1f0eb492474c6477f839d
Gary Engler <gary.engler@capco.com>
2020-01-07 12:38:28 -0500

    Added PLANT CLEF DATA env var

--- run_docker.sh
+++ run_docker.sh
@@ -3,5 +3,5 @@ docker pull gengler1123/triplet_loss_demo:latest
 docker run \
   -p 8888:8888 \
   -v "$PWD"/notebooks:/home/jovyan/work \
-  -v /Users/gengler/data:/data \
+  -v $PLANT_CLEF_DATA:/data \
   gengler1123/triplet_loss_demo:latest
